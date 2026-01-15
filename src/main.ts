73a3ff11f2c6bf6eef9050a964245fe161c1d5fc
natcore-tech <asesoriaelectr@gmail.com>
2026-01-15 15:24:10 -0500

    add app enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // Vite
+      'http://localhost:3000', // si tu front usa este
+      'https://post-api.nael.live',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // si usas cookies/sesión
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
   app.useStaticAssets(join(__dirname, '..', 'public'),
