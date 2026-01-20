5b06ba110311a7e232904aa117253c4bfe86e308
natcore-tech <asesoriaelectr@gmail.com>
2026-01-20 08:18:53 -0500

    update login dto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,8 @@
 import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDto {
-  @IsString()
-  username: string;
-
   @IsEmail()
-  email: string;
+  username: string;
 
   @IsString()
   password: string;
